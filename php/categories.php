8ddf4a200d99de810fd8ecc1a0385b93f6d07b1c
ryanvanwagoner <36165697+ryanvanwagoner@users.noreply.github.com>
2018-02-06 11:49:00 -0700

    feb6 morning changes

--- php/categories.php
+++ php/categories.php
@@ -2,22 +2,39 @@
 <!doctype html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <title>SellOut | Home</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
-        <link rel="stylesheet" href="./styles/styles.css" >
+    <title>Categories</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../css/styles.css" > 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+</head>
 
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
-    </head>
-    <body style="background: #E4E5E7 !important;">       
-        <?php include("header.php"); ?>
+<body id ="Categories">
+
+	<!-- Navbar -->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="recipelist.php"><span class="glyphicon glyphicon-home"></span></a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="categories.php">Categories</a></li>
+			<li><a href="addrecipe.php">Add New Recipe</a></li>
+			<li><a href="login.php">Sign In</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<div class="jumbotron text-center">
+		<h1>Categories</h1> 
+	</div>
+
+     
         <div class="container categories-container"  >
-            <h1 class="home-title" style="letter-spacing: 2px;">Categories</h1>
+            <!--<h1 class="home-title" style="letter-spacing: 2px;">Categories</h1>-->
             <div class="container" style="max-width: 100% !important">                
                 <div class="row form-row" >
                     <form class="form-inline">
@@ -28,6 +45,7 @@
                                 <option>Pork</option>
                                 <option>Quick and Easy</option>
                                 <option>Oriental</option>
+                                <option>Other</option>
                                
                             </select>
                         </div>
@@ -44,6 +62,5 @@
             </div>
         </div>
     </body>
-    <?php include("footer.php"); ?>
 
 </html>
--- php/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <nav class="navbar fixed-bottom navbar-light bg-light" style="background: transparent !important; justify-content: center">
-        <ul class="navbar-nav" >
-            <li class="nav-item">
-                <a class="nav-link footer-link" href="#">Property Of Student Eats. All rights reserved</a>
-            </li>
-        </ul>
-    </nav>
-</footer>
